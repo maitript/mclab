db25dbe150d37531d95c6eac5bf190f309b41825
Maitri P Tadas <93444567+maitript@users.noreply.github.com>
2022-08-07 14:28:18 +0530

    Update partb_q3.c

--- partb_q3.c
+++ partb_q3.c
@@ -1,20 +1,20 @@
 #include<lpc214x.h>
 unsigned int x=0;
-__irq void Timer0_ISR()
+__irq void Timer0_ISR(void)
 {
 	x^=1;
 	if(x)
-		IOSET1=0xFFFFFFFF;//1<<20;better make all pins as output so u dont have find in logic analyser
+		IOSET1= 1<<20;
 	else
-		IOCLR1=0XFFFFFFFF;//1<<20;
+		IOCLR1= 1<<20;
 	T0IR=0x01;
-	VICVectAddr=0x0000000h;
+	VICVectAddr=0x0;
 }
 int main()
 {
 	IODIR1=0xffffffff;
 	T0MCR=0x03;
-	T0MR0=0x3456ff;
+	T0MR0=0x3456fff;
 	VICVectAddr4=(unsigned)Timer0_ISR;
 	VICVectCntl4=0x24;
 	VICIntEnable=0x10;
